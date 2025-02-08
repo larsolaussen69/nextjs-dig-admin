2237a65e06425e72194a97e99c6cb97b91ddf187
larsolaussen <33283679+larsolaussen@users.noreply.github.com>
2025-02-08 17:10:14 +0100

    ..

--- src/app/api/d/route.ts
+++ src/app/api/d/route.ts
@@ -1,9 +1,21 @@
-export async function GET() {
-    return new Response("hei",
-      // JSON.stringify({
-      //   databaseUrl: process.env.DATABASE_URL ? "Exists" : "Missing",
-      //   nextAuthSecret: process.env.NEXTAUTH_SECRET ? "Exists" : "Missing",
-      // }),
-      { status: 200 }
-    );
-  };
+
+import { getUserExercisesByEmail } from '@/lib/sql/user-exercises';
+import { NextResponse } from 'next/server';
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const email = searchParams.get('email');
+
+  // Optionally, validate the user ID or authenticate the request here
+  if (!email) {
+    return NextResponse.json({ success: false, error: 'User email is required' }, { status: 400 });
+  }
+
+  try {
+    const dbUser = await getUserExercisesByEmail(email);
+        return NextResponse.json({ success: true, data: dbUser });
+  } catch (error) {
+    console.error('Error fetching user:', error);
+    return NextResponse.json({ success: false, error: 'Failed to fetch user' }, { status: 500 });
+  }
+}
