315765428253aeb61ae4164e041d42708d058ed7
larsolaussen <33283679+larsolaussen@users.noreply.github.com>
2025-02-08 17:01:55 +0100

    c

--- src/app/api/d/route.ts
+++ src/app/api/d/route.ts
@@ -1,9 +1,9 @@
 export async function GET() {
-    return new Response(
-      JSON.stringify({
-        databaseUrl: process.env.DATABASE_URL ? "Exists" : "Missing",
-        nextAuthSecret: process.env.NEXTAUTH_SECRET ? "Exists" : "Missing",
-      }),
+    return new Response("hei",
+      // JSON.stringify({
+      //   databaseUrl: process.env.DATABASE_URL ? "Exists" : "Missing",
+      //   nextAuthSecret: process.env.NEXTAUTH_SECRET ? "Exists" : "Missing",
+      // }),
       { status: 200 }
     );
   };
