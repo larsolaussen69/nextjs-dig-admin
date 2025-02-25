f119b84da0f926663cc80a17ba8be62255f58c58
larsolaussen <33283679+larsolaussen@users.noreply.github.com>
2025-02-25 10:31:40 +0100

    added api key

--- src/app/api/user/exercises/route.ts
+++ src/app/api/user/exercises/route.ts
@@ -10,7 +10,15 @@ export async function GET(request: Request) {
   if (!email) {
     return NextResponse.json({ success: false, error: 'User email is required' }, { status: 400 });
   }
-
+  const apiKey = request.headers.get('x-api-key');
+  const validApiKey = process.env.API_SECRET;
+  
+  if (apiKey !== validApiKey) {
+    return new Response(JSON.stringify({ error: 'Unauthorized' }), {
+      status: 401,
+      headers: { 'Content-Type': 'application/json' }
+    });
+  }
   try {
     const dbUser = await getUserExercisesByEmail(email);
         return NextResponse.json({ success: true, data: dbUser });
